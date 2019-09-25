9b514c60938ed637d2365ec4b6f940bc00c5c550
Alan Dodge <alphaechododge@gmail.com>
2019-09-25 16:11:34 +0000

    Tested moving, moved naming into same 'connect' callback as connection message

--- client.js
+++ client.js
@@ -9,15 +9,23 @@ const connect = function() {
     port: 50541
   });
 
-  // notify that we are connected
+  // create 'connect' callback
   conn.on('connect', () => {
+    // notify we are connected
     console.log("We are connected!");
-  });
-
-  // set snek name
-  conn.on('connect', () => {
+    // set snek name on server
     conn.write('Name: BFS');
   });
+
+  // // move
+  // conn.on('connect', () => {
+  //   for (let i = 0; i < 4; i++) {
+  //     setTimeout(() => {
+  //       conn.write('Move: up');
+
+  //     }, i*100);
+  //   }    
+  // });
 
   // interpret incoming data as text
   conn.setEncoding('utf8');
