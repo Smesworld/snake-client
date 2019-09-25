201ede06d1d4c3bd82ce6fe69afbb9ecce72faf0
Alan Dodge <alphaechododge@gmail.com>
2019-09-25 16:20:50 +0000

    Initiated user input and handled ctrl + C to quit

--- play.js
+++ play.js
@@ -1,4 +1,29 @@
+//Import required functions
 const { connect } = require('./client');
 
+//Establish connection
 console.log('Connecting ...');
 connect();
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+
+  return stdin;
+}
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    console.log(`Thanks for using me, bie!`);
+    process.exit();
+  }
+}
+
+setupInput();
