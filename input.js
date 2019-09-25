18f86848268da75c81c0d04523d358b7a48734b1
Alan Dodge <alphaechododge@gmail.com>
2019-09-25 16:33:47 +0000

    Added movement keys to input

--- input.js
+++ input.js
@@ -16,6 +16,14 @@ const handleUserInput = function(key) {
   if (key === '\u0003') {
     console.log(`Thanks for using me, bie!`);
     process.exit();
+  } else if (key === "w") {
+    console.log(`Key: ${key}, move up!`)
+  } else if (key === "a") {
+    console.log(`Key: ${key}, move left!`);
+  } else if (key === "s") {
+    console.log(`Key: ${key}, move down`);
+  } else if (key === "d") {
+    console.log(`Key: ${key}, move right`);
   }
 }
 
